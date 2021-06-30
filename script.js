e9948ded3a60a879aae20f4aa993bde608cd070a
Nick Leon <nicolas7@vt.edu>
2021-06-30 10:46:40 -0400

    Add data persistance for each hourblock when save is pushed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var container = $('#container')
 var timeEl = $('#currentDay')
+var textarray = ["","","","","","","","",""];
 // var timeRef = moment().format()
 timeEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
@@ -12,57 +13,43 @@ function timer(){
 render()
 
 function render(){
-for(let i=0; i<9; i++){
-    var timeRef = moment().format()
-    // console.log(timeRef)
-    var track1 = timeRef.charAt(17)
-    var track2 = timeRef.charAt(18)
-    // console.log(track1 + track2)
-    var compare = track1 + track2
-    var blockContainer = $('<div>')
-    var blocklTitle = $('<h1>')
-    var blockText = $('<textarea>')
-    var blockButton = $('<button>')
+    for(let i=0; i<9; i++){
 
-    blockContainer.attr('id', 'hourblock')
-    blockContainer.addClass('blockcontainer')
-    blocklTitle.attr('id', 'test')
-    blockText.addClass('form-control')
-    blockText.attr('id', 'exampleFormControlTextarea1')
-    blockText.attr('rows', '3')
-    blockButton.addClass('btn btn-primary mb-3')
-    blockButton.text('save')
-
-    check()
-    setInterval(check,1000)
-
-    function check(){
-
-    // if ((i+1)*10<compare){
-    //     blockContainer.css("background-color","yellow")
+        var storedtextarry = JSON.parse(localStorage.getItem("data"))
+        if(storedtextarry !== null){
+            textarray = storedtextarry
+        }
         
-    // }
-    // else{
-    //     blockContainer.css("background-color","white")
-      
-    // }
-}
-
-    if(i>3){
-        blocklTitle.text(i-3 + " AM")
+        var blockContainer = $('<div>')
+        var blocklTitle = $('<h1>')
+        var blockText = $('<textarea>')
+        var blockButton = $('<button>')
+        
+        blockContainer.attr('id', 'hourblock')
+        blockContainer.addClass('blockcontainer')
+        blocklTitle.attr('id', 'test')
+        blockText.addClass('form-control')
+        blockText.attr('id', 'exampleFormControlTextarea1')
+        blockText.attr('rows', '3')
+        blockText.text(textarray[i])
+        blockButton.attr('id','savebutton')
+        blockButton.addClass('btn btn-primary mb-3')
+        blockButton.text('save')
+        
+        if(i>3){
+            blocklTitle.text(i-3 + " AM")
+        }
+        else{
+            blocklTitle.text(i+9 + " AM")
+        }
+        
+        blockContainer.append(blocklTitle)
+        blockContainer.append(blockText)
+        blockContainer.append(blockButton)
+        container.append(blockContainer)
     }
-    else{
-    blocklTitle.text(i+9 + " AM")
-    }
-
-    // console.log('asdf')
-
-    blockContainer.append(blocklTitle)
-    blockContainer.append(blockText)
-    blockContainer.append(blockButton)
-    container.append(blockContainer)
 }
-}
+
 
 let n = 1;
 let elementCall = "div:nth-child(" + n + ")"
@@ -72,31 +59,48 @@ let compare = track1 + track2
 
 color()
 setInterval(color, 1000)
-// setInterval(color, 1000)
-function color(){
-    // console.log('TEST')
-for (n = 1; n < 10; n++) {
-    timeRef = moment().format()
-    // console.log(timeRef)
-    track1 = timeRef.charAt(17)
-    track2 = timeRef.charAt(18)
-    // console.log(track1 + track2)
-    compare = track1 + track2
-    console.log(compare)
-    elementCall = "div:nth-child(" + n + ")"
-    console.log(elementCall)
-    console.log(compare)
 
-    console.log(n*10)
-    if (n * 10 < compare) {
-        console.log("true")
-        $(elementCall).css("background-color", "yellow")
-        container.css("background-color", "white")
-    }
-    else {
-        $(elementCall).css("background-color", "white")
-        container.css("background-color", "white")
-    }
+function color(){
     
+    for (n = 1; n < 10; n++) {
+        timeRef = moment().format()
+        track1 = timeRef.charAt(17)
+        track2 = timeRef.charAt(18)
+        compare = track1 + track2
+        elementCall = "div:nth-child(" + n + ")"
+        
+        if (n * 10 <= compare) {
+            
+            $(elementCall).css("background-color", "yellow")
+            container.css("background-color", "white")
+        }
+        else {
+            $(elementCall).css("background-color", "white")
+            container.css("background-color", "white")
+        }
+        
+    }
 }
-}
+
+var button = $('button')
+var content = $('#exampleFormControlTextarea1')
+// button.on('click', function () {
+    //     console.log("asdfa")
+    // });
+    
+    
+button.on("click", function (event) {
+    event.preventDefault()
+    var test = $(this).siblings('textarea').val();
+    var blocknum = $(this).parent().index()
+    console.log(test)
+    console.log(blocknum)
+
+    textarray.splice(blocknum, 1, test)
+    console.log(textarray)
+    
+    localStorage.setItem("data",JSON.stringify(textarray))
+
+})
+
+    // var set = localStorage.getItem("data")
